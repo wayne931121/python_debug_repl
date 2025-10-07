275aef7f297e814519535d5b02c1a92ef00a6517
Wayne咏辰 <75261164+wayne931121@users.noreply.github.com>
2025-10-07 21:25:16 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+
+# print(find_packages())
+
+setup(
+    name='cdebug',
+    version='0.0.1',
+    author='wayne931121',
+    author_email='',
+    description='debug',
+    license='CC-BY-NC 4.0',
+    url='https://github.com/wayne931121',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.0',
+)
