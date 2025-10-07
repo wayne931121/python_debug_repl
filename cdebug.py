ada5b53cbd75ff02da98bb48845bbe6f663d0222
Wayne咏辰 <75261164+wayne931121@users.noreply.github.com>
2025-10-07 15:09:37 +0800

    Update cdebug.py

--- cdebug.py
+++ cdebug.py
@@ -1,11 +1,11 @@
 import os, time
 while 1:
     try:
-        cd = input("$cdebug" + time.strftime("%Y%m%d-%H%M%S", time.localtime()) + ": " )
+        cd = input("\033[1;32m$cdebug" + time.strftime("%Y%m%d-%H%M%S", time.localtime()) + ": \033[00m" )
         if cd=="exit":
              break
         cd = cd.replace("\\br","\n")
-        if len(cd) and cd[0:1]=="!":
+        if cd and cd[0:1]=="!":
             os.system(cd[1:])
         else:
             exec(cd, globals())
