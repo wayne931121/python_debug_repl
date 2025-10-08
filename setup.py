0adc1d683570762644dea3c3b737ef6be98733e4
Wayne咏辰 <75261164+wayne931121@users.noreply.github.com>
2025-10-08 19:44:52 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 from setuptools import setup,find_packages
 
 # print(find_packages())
+with open("README.md",encoding="utf-8") as f:
+    md = f.read()
+
 
 setup(
-    name='cdebug',
-    version='0.0.1',
-    author='wayne931121',
-    author_email='',
-    description='debug',
-    license='CC-BY-NC 4.0',
-    url='https://github.com/wayne931121',
+    name="cdebug",
+    version="0.0.1",
+    author="wayne931121",
+    author_email="",
+    description="debug",
+    long_description=md,
+    long_description_content_type="text/markdown",
+    license="CC-BY-NC 4.0",
+    url="https://github.com/wayne931121",
     packages=find_packages(),
     install_requires=[],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.0',
+    python_requires=">=3.0",
 )
